fe8fbdabb86831b3a13ac13d9859effbd5f2ffe5
luxuncang <luxuncang@qq.com>
2021-11-24 16:37:44 +0800

    https://github.com/GraiaProject/Ariadne/pull/26#issuecomment-977529090
    Change reload interface
    Add example for Saya.
    Add dependencies to pyproject.toml.

--- src/graia/ariadne/app.py
+++ src/graia/ariadne/app.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+import os
 from watchgod import arun_process
 from asyncio.events import AbstractEventLoop
 from asyncio.exceptions import CancelledError
@@ -1108,6 +1109,11 @@ class MultimediaMixin(AriadneMixin):
 
 T = TypeVar("T")
 
+reloadApp = None
+
+def reloadfun():
+    logger.info("Reloading Ariadne...")
+    asyncio.get_event_loop().run_until_complete(reloadApp.lifecycle())
 
 class Ariadne(
     MessageMixin, RelationshipMixin, OperationMixin, FileMixin, MultimediaMixin
@@ -1161,9 +1167,10 @@ class Ariadne(
             if isinstance(connect_info, Adapter)
             else DefaultAdapter(self.broadcast, connect_info)
         )
+        global reloadApp
+        reloadApp = self
         self.adapter.app = self
         self.mirai_session: MiraiSession = self.adapter.mirai_session
-
         self.daemon_task: Optional[Task] = None
         self.running: bool = False
         self.remote_version: str = ""
@@ -1296,13 +1303,15 @@ class Ariadne(
         except CancelledError:
             pass
         await self.stop()
-        
+
     @staticmethod
     async def watchfile(reload: Callable[[], None]) -> None:
+        logger.info("Watching file changes...")
         await arun_process(os.getcwd(), reload)
 
-    def run(self, reload: Callable[[], None]) -> None:
-        self.loop.create_task(self.watchfile(reload))
+    def run(self, reload: bool = False) -> None:
+        if reload:
+            self.loop.create_task(self.watchfile(reloadfun))
         self.loop.run_forever()
 
     @app_ctx_manager

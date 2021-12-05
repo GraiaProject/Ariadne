f63e586e71fb9cdf5b3fea83e464b27a7ccd9d72
BlueGlassBlock <blueglassblock@outlook.com>
2021-12-05 18:13:54 +0800

    provided search function for docs

--- dev_utils/build_docs.py
+++ dev_utils/build_docs.py
@@ -10,7 +10,18 @@ try:
 except FileNotFoundError:
     pass
 os.chdir(os.path.abspath(os.path.join(__file__, "..", "..", "src", "graia")))
-subprocess.run(["pdoc", "--html", "ariadne", "--force", "-o", "./../.."])
+subprocess.run(
+    [
+        "pdoc",
+        "--html",
+        "ariadne",
+        "--force",
+        "-o",
+        "./../..",
+        "--config",
+        "lunr_search={'fuzziness': 1, 'index_docstrings': True}",
+    ]
+)
 os.chdir(os.path.abspath(os.path.join(__file__, "..", "..")))
 os.rename("ariadne", "docs")
 opt = input("Confirm to publish?")

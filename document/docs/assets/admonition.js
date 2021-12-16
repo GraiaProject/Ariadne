2e56dc0be59fa356eeaaf6e744ee4f2593977620
BlueGlassBlock <blueglassblock@outlook.com>
2021-12-16 14:58:25 +0800

    improce document

---
+++ document/docs/assets/admonition.js
@@ -0,0 +1,11 @@
+
+const admonition_titles = document.getElementsByClassName("admonition-title")
+for (let i = 0; i < admonition_titles.length; i++) {
+    let color = window.getComputedStyle(admonition_titles[i]).borderColor
+    admonition_titles[i].style.color = color
+}
+const admonition_summaries = document.getElementsByTagName("SUMMARY")
+for (let i = 0; i < admonition_summaries.length; i++) {
+    let color = window.getComputedStyle(admonition_summaries[i]).borderColor
+    admonition_summaries[i].style.color = color
+}

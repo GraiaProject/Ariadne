5a0e7d700a59c505b372225ca852e7ddfa96cfe8
GreyElaina <GreyElaina@outlook.com>
2022-01-27 23:48:25 +0800

    fit pylance

--- src/graia/ariadne/entry/__init__.py
+++ src/graia/ariadne/entry/__init__.py
@@ -2,46 +2,55 @@
 
 # no error
 
-from ..adapter import CombinedAdapter, DefaultAdapter, HttpAdapter, WebsocketAdapter
-from ..app import Ariadne
-from ..context import (
-    adapter_ctx,
-    ariadne_ctx,
-    broadcast_ctx,
-    event_ctx,
-    event_loop_ctx,
-    upload_method_ctx,
+from ..adapter import (
+    CombinedAdapter as CombinedAdapter,
+    DefaultAdapter as DefaultAdapter,
+    HttpAdapter as HttpAdapter,
+    WebsocketAdapter as WebsocketAdapter,
 )
-from ..dispatcher import ContextDispatcher, MessageChainDispatcher, SourceDispatcher
+from ..app import Ariadne as Ariadne
+from ..context import (
+    adapter_ctx as adapter_ctx,
+    ariadne_ctx as ariadne_ctx,
+    broadcast_ctx as broadcast_ctx,
+    event_ctx as event_ctx,
+    event_loop_ctx as event_loop_ctx,
+    upload_method_ctx as upload_method_ctx,
+)
+from ..dispatcher import (
+    ContextDispatcher as ContextDispatcher,
+    MessageChainDispatcher as MessageChainDispatcher,
+    SourceDispatcher as SourceDispatcher,
+)
 from ..exception import (
-    AccountMuted,
-    AccountNotFound,
-    InvalidArgument,
-    InvalidEventTypeDefinition,
-    InvalidSession,
-    InvalidVerifyKey,
-    MessageTooLong,
-    UnknownError,
-    UnknownTarget,
-    UnVerifiedSession,
+    AccountMuted as AccountMuted,
+    AccountNotFound as AccountNotFound,
+    InvalidArgument as InvalidArgument,
+    InvalidEventTypeDefinition as InvalidEventTypeDefinition,
+    InvalidSession as InvalidSession,
+    InvalidVerifyKey as InvalidVerifyKey,
+    MessageTooLong as MessageTooLong,
+    UnknownError as UnknownError,
+    UnknownTarget as UnknownTarget,
+    UnVerifiedSession as UnVerifiedSession,
 )
 from ..model import (
-    BotMessage,
-    CallMethod,
-    ChatLogConfig,
-    Client,
-    DownloadInfo,
-    FileInfo,
-    Friend,
-    Group,
-    GroupConfig,
-    Member,
-    MemberInfo,
-    MemberPerm,
-    MiraiSession,
-    Profile,
-    Stranger,
-    UploadMethod,
+    BotMessage as BotMessage,
+    CallMethod as CallMethod,
+    ChatLogConfig as ChatLogConfig,
+    Client as Client,
+    DownloadInfo as DownloadInfo,
+    FileInfo as FileInfo,
+    Friend as Friend,
+    Group as Group,
+    GroupConfig as GroupConfig,
+    Member as Member,
+    MemberInfo as MemberInfo,
+    MemberPerm as MemberPerm,
+    MiraiSession as MiraiSession,
+    Profile as Profile,
+    Stranger as Stranger,
+    UploadMethod as UploadMethod,
 )
-from ..util import cpu_bound, io_bound
-from . import event, message
+from ..util import cpu_bound as cpu_bound, io_bound as io_bound
+from . import event as event, message as message
--- src/graia/ariadne/entry/message.py
+++ src/graia/ariadne/entry/message.py
@@ -2,52 +2,52 @@
 
 # no error
 
-from ..message.chain import MessageChain
-from ..message.commander import Arg, Commander, Slot
-from ..message.component import Component
+from ..message.chain import MessageChain as MessageChain
+from ..message.commander import Arg as Arg, Commander as Commander, Slot as Slot
+from ..message.component import Component as Component
 from ..message.element import (
-    App,
-    At,
-    AtAll,
-    Dice,
-    Element,
-    Face,
-    File,
-    FlashImage,
-    Forward,
-    ForwardNode,
-    Image,
-    ImageType,
-    MultimediaElement,
-    MusicShare,
-    NotSendableElement,
-    Plain,
-    Poke,
-    PokeMethods,
-    Quote,
-    Source,
-    Voice,
+    App as App,
+    At as At,
+    AtAll as AtAll,
+    Dice as Dice,
+    Element as Element,
+    Face as Face,
+    File as File,
+    FlashImage as FlashImage,
+    Forward as Forward,
+    ForwardNode as ForwardNode,
+    Image as Image,
+    ImageType as ImageType,
+    MultimediaElement as MultimediaElement,
+    MusicShare as MusicShare,
+    NotSendableElement as NotSendableElement,
+    Plain as Plain,
+    Poke as Poke,
+    PokeMethods as PokeMethods,
+    Quote as Quote,
+    Source as Source,
+    Voice as Voice,
 )
-from ..message.formatter import Formatter
-from ..message.parser.base import DetectPrefix, DetectSuffix
+from ..message.formatter import Formatter as Formatter
+from ..message.parser.base import DetectPrefix as DetectPrefix, DetectSuffix as DetectSuffix
 from ..message.parser.literature import (
-    BoxParameter,
-    Literature,
-    ParamPattern,
-    SwitchParameter,
+    BoxParameter as BoxParameter,
+    Literature as Literature,
+    ParamPattern as ParamPattern,
+    SwitchParameter as SwitchParameter,
 )
 from ..message.parser.twilight import (
-    FORCE,
-    NOSPACE,
-    PRESERVE,
-    ArgumentMatch,
-    ElementMatch,
-    FullMatch,
-    Match,
-    RegexMatch,
-    Sparkle,
-    Twilight,
-    UnionMatch,
-    WildcardMatch,
+    FORCE as FORCE,
+    NOSPACE as NOSPACE,
+    PRESERVE as PRESERVE,
+    ArgumentMatch as ArgumentMatch,
+    ElementMatch as ElementMatch,
+    FullMatch as FullMatch,
+    Match as Match,
+    RegexMatch as RegexMatch,
+    Sparkle as Sparkle,
+    Twilight as Twilight,
+    UnionMatch as UnionMatch,
+    WildcardMatch as WildcardMatch,
 )
-from ..util.send import Bypass, Ignore, Safe, Strict
+from ..util.send import Bypass as Bypass, Ignore as Ignore, Safe as Safe, Strict as Strict

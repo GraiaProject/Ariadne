94fc1008973cbdd0adf4a5d842f125b5a784cbbf
Tarrailt <3165388245@qq.com>
2022-03-23 20:47:32 +0800

    Update alconna.py

--- src/graia/ariadne/message/parser/alconna.py
+++ src/graia/ariadne/message/parser/alconna.py
@@ -2,6 +2,11 @@
 该文件仅作为兼容用
 """
 
-from arclet.alconna.graia import Alconna as Alconna
-from arclet.alconna.graia import AlconnaDispatcher as AlconnaDispatcher
-from arclet.alconna.graia import AlconnaHelpMessage as AlconnaHelpMessage
+from arclet.alconna.graia import Alconna as _Alc
+from arclet.alconna.graia import AlconnaDispatcher as _Alc_Disp
+from arclet.alconna.graia import AlconnaHelpMessage as _Alc_Hlp
+
+Alconna = _Alc
+AlconnaDispatcher = _Alc_Disp
+AlconnaHelpMessage = _Alc_Hlp
+

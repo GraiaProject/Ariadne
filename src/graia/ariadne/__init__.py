fa555fd42e1a59a36d5332545f0df4f63aacd58e
BlueGlassBlock <blueglassblock@outlook.com>
2022-05-12 21:05:09 +0800

    :label: fix get_running type hint

--- src/graia/ariadne/__init__.py
+++ src/graia/ariadne/__init__.py
@@ -13,7 +13,7 @@ from .typing import T
 
 
 @overload
-def get_running(type: Type[T] = Ariadne) -> T:
+def get_running() -> Ariadne:
     ...
 
 
@@ -23,7 +23,7 @@ def get_running(type: Type[T], fail_err: Literal[False]) -> Optional[T]:
 
 
 @overload
-def get_running(type: Type[T], fail_err: Literal[True]) -> T:
+def get_running(type: Type[T], fail_err: Literal[True] = True) -> T:
     ...
 
 

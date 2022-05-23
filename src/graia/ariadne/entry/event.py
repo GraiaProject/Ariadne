83288c1d6cac7d6157ce7c95870bc7e413ebcf3c
BlueGlassBlock <blueglassblock@outlook.com>
2022-05-23 18:47:33 +0800

    :construction: change entry import style

--- src/graia/ariadne/entry/event.py
+++ src/graia/ariadne/entry/event.py
@@ -1,5 +1,64 @@
 from ..event import MiraiEvent as MiraiEvent
 from ..event.lifecycle import ApplicationLaunched as ApplicationLaunched
 from ..event.lifecycle import ApplicationShutdowned as ApplicationShutdowned
-from ..event.message import *
-from ..event.mirai import *
+from ..event.message import ActiveFriendMessage as ActiveFriendMessage
+from ..event.message import ActiveGroupMessage as ActiveGroupMessage
+from ..event.message import ActiveMessage as ActiveMessage
+from ..event.message import ActiveStrangerMessage as ActiveStrangerMessage
+from ..event.message import ActiveTempMessage as ActiveTempMessage
+from ..event.message import FriendMessage as FriendMessage
+from ..event.message import FriendSyncMessage as FriendSyncMessage
+from ..event.message import GroupMessage as GroupMessage
+from ..event.message import GroupSyncMessage as GroupSyncMessage
+from ..event.message import MessageEvent as MessageEvent
+from ..event.message import StrangerMessage as StrangerMessage
+from ..event.message import StrangerSyncMessage as StrangerSyncMessage
+from ..event.message import SyncMessage as SyncMessage
+from ..event.message import TempMessage as TempMessage
+from ..event.message import TempSyncMessage as TempSyncMessage
+from ..event.mirai import BotEvent as BotEvent
+from ..event.mirai import BotGroupPermissionChangeEvent as BotGroupPermissionChangeEvent
+from ..event.mirai import (
+    BotInvitedJoinGroupRequestEvent as BotInvitedJoinGroupRequestEvent,
+)
+from ..event.mirai import BotJoinGroupEvent as BotJoinGroupEvent
+from ..event.mirai import BotLeaveEventActive as BotLeaveEventActive
+from ..event.mirai import BotLeaveEventKick as BotLeaveEventKick
+from ..event.mirai import BotMuteEvent as BotMuteEvent
+from ..event.mirai import BotOfflineEventActive as BotOfflineEventActive
+from ..event.mirai import BotOfflineEventDropped as BotOfflineEventDropped
+from ..event.mirai import BotOfflineEventForce as BotOfflineEventForce
+from ..event.mirai import BotOnlineEvent as BotOnlineEvent
+from ..event.mirai import BotReloginEvent as BotReloginEvent
+from ..event.mirai import BotUnmuteEvent as BotUnmuteEvent
+from ..event.mirai import ClientKind as ClientKind
+from ..event.mirai import CommandExecutedEvent as CommandExecutedEvent
+from ..event.mirai import FriendEvent as FriendEvent
+from ..event.mirai import FriendInputStatusChangedEvent as FriendInputStatusChangedEvent
+from ..event.mirai import FriendNickChangedEvent as FriendNickChangedEvent
+from ..event.mirai import FriendRecallEvent as FriendRecallEvent
+from ..event.mirai import GroupAllowAnonymousChatEvent as GroupAllowAnonymousChatEvent
+from ..event.mirai import GroupAllowConfessTalkEvent as GroupAllowConfessTalkEvent
+from ..event.mirai import GroupAllowMemberInviteEvent as GroupAllowMemberInviteEvent
+from ..event.mirai import (
+    GroupEntranceAnnouncementChangeEvent as GroupEntranceAnnouncementChangeEvent,
+)
+from ..event.mirai import GroupEvent as GroupEvent
+from ..event.mirai import GroupMuteAllEvent as GroupMuteAllEvent
+from ..event.mirai import GroupNameChangeEvent as GroupNameChangeEvent
+from ..event.mirai import GroupRecallEvent as GroupRecallEvent
+from ..event.mirai import MemberCardChangeEvent as MemberCardChangeEvent
+from ..event.mirai import MemberHonorChangeEvent as MemberHonorChangeEvent
+from ..event.mirai import MemberJoinEvent as MemberJoinEvent
+from ..event.mirai import MemberJoinRequestEvent as MemberJoinRequestEvent
+from ..event.mirai import MemberLeaveEventKick as MemberLeaveEventKick
+from ..event.mirai import MemberLeaveEventQuit as MemberLeaveEventQuit
+from ..event.mirai import MemberMuteEvent as MemberMuteEvent
+from ..event.mirai import MemberPermissionChangeEvent as MemberPermissionChangeEvent
+from ..event.mirai import MemberSpecialTitleChangeEvent as MemberSpecialTitleChangeEvent
+from ..event.mirai import MemberUnmuteEvent as MemberUnmuteEvent
+from ..event.mirai import NewFriendRequestEvent as NewFriendRequestEvent
+from ..event.mirai import NudgeEvent as NudgeEvent
+from ..event.mirai import OtherClientOfflineEvent as OtherClientOfflineEvent
+from ..event.mirai import OtherClientOnlineEvent as OtherClientOnlineEvent
+from ..event.mirai import RequestEvent as RequestEvent

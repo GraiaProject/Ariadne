42d8646310a3fd2119ffb51e43e3f6c2dfe9484e
BlueGlassBlock <blueglassblock@outlook.com>
2022-06-01 13:05:09 +0800

    :book: try to fix readthedocs build

--- docs/gen_ref.py
+++ docs/gen_ref.py
@@ -22,7 +22,7 @@ for path in sorted(Path(root, "src", "graia").glob("**/*.py")):
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
+    elif parts[-1] == "__main__" or parts[-1].startswith("_"):
         continue
     nav[parts] = doc_path
 

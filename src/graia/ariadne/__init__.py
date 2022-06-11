74f9371bff4e7f674a2206c78328b51328496276
BlueGlassBlock <blueglassblock@outlook.com>
2022-06-11 23:41:35 +0800

    :headstone: R.I.P. graia-application-mirai

--- src/graia/ariadne/__init__.py
+++ src/graia/ariadne/__init__.py
@@ -3,5 +3,9 @@
 一个优雅的 QQ Bot 框架.
 """
 
-from . import entry as entry  # noqa: F401
+from importlib.metadata import Distribution
+
 from .app import Ariadne as Ariadne  # noqa: F401
+
+if next(iter(Distribution.discover(name="graia-application-mirai")), None) is not None:
+    raise ImportError("`graia-application-mirai` is out of support.")

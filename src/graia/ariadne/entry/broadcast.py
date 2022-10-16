45f664f6e619f15582bdf6f203eb2e9b6ad690a7
ProgramRipper <programripper@foxmail.com>
2022-10-16 21:11:39 +0800

    fix: add entry for ExceptionThrown

--- src/graia/ariadne/entry/broadcast.py
+++ src/graia/ariadne/entry/broadcast.py
@@ -1,6 +1,8 @@
 from graia.broadcast import Broadcast as Broadcast
 from graia.broadcast.builtin.decorators import Depend as Depend
+from graia.broadcast.builtin.event import EventExceptionThrown as EventExceptionThrown
 from graia.broadcast.builtin.event import ExceptionThrowed as ExceptionThrowed
+from graia.broadcast.builtin.event import ExceptionThrown as ExceptionThrown
 from graia.broadcast.exceptions import ExecutionStop as ExecutionStop
 from graia.broadcast.exceptions import PropagationCancelled as PropagationCancelled
 from graia.broadcast.interrupt import InterruptControl as InterruptControl
@@ -9,7 +11,9 @@ from graia.broadcast.interrupt import Waiter as Waiter
 __all__ = [
     "Broadcast",
     "Depend",
+    "EventExceptionThrown",
     "ExceptionThrowed",
+    "ExceptionThrown",
     "ExecutionStop",
     "InterruptControl",
     "PropagationCancelled",

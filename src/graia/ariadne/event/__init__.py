ca6d978c971d945612724862e32c953562a91901
ProgramRipper <programripper@foxmail.com>
2023-02-14 21:39:53 +0800

    fix: remove MiraiEvent.validate_event_type()
    fix #216

--- src/graia/ariadne/event/__init__.py
+++ src/graia/ariadne/event/__init__.py
@@ -1,10 +1,7 @@
 """Ariadne 的事件"""
-from pydantic import validator
-
 from graia.broadcast import Dispatchable
 
 from ..dispatcher import BaseDispatcher
-from ..exception import InvalidEventTypeDefinition
 from ..model import AriadneBaseModel
 
 
@@ -13,15 +10,6 @@ class MiraiEvent(Dispatchable, AriadneBaseModel):
 
     type: str
     """事件类型"""
-
-    @validator("type", allow_reuse=True)
-    def validate_event_type(cls, v):
-        """验证事件类型, 通过比对 type 字段实现"""
-        if not isinstance(cls, type):
-            raise TypeError("cls must be a class!")
-        if cls.type != v:
-            raise InvalidEventTypeDefinition(f"{cls.__name__}'s type must be '{cls.type}', not '{v}'")
-        return v
 
     Dispatcher = BaseDispatcher
 
